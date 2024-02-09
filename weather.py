d7bad23126769376dab66833832b464352274841
MaksymilianKa <159538843+MaksymilianKa@users.noreply.github.com>
2024-02-09 15:16:10 +0100

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,5 @@
+weather=int(input("Podaj temperature w stopni celsjusza: "))
+if weather > -5 and weather < 35:
+    print("Spokojnie możesz wychodzić na spacer")
+else:
+    print("Pogoda nie jest dobra na spacer, odpuść sobie")
